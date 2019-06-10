9c8408809cc83a5a2922ffa182863df95708cfc7
Alexander Ding <alexding123@outlook.com>
2019-06-10 14:57:07 -0400

    OpenMP update

--- openmp/graph.cpp
+++ openmp/graph.cpp
@@ -6,29 +6,80 @@ Graph::Graph(char* path) {
     int n, m;
     fin >> n >> m;
 
+    vector<int> temp_offsets(n);
+    vector<int> temp_edges(m);
     int offset, edge; 
     for (int i = 0; i < n; i++) {
         fin >> offset; 
-        this->offsets.push_back(offset);
+        temp_offsets[i] = offset;
     }
 
     for (int i = 0; i < m; i++) {
         fin >> edge; // edge from current_node to node edge
-        this->edges.push_back(edge);
+        temp_edges[i] = edge;
+    }
+
+    vector<vector<int>> edges_out(n); 
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        int limit = (i==(n-1)) ? (m-index) : (temp_offsets[i+1]-temp_offsets[i]);
+
+        for (int j = 0; j < limit; j++) {
+            edges_out[i].push_back(temp_edges[index]);
+            index++;
+        }
+    }
+
+    vector<vector<int>> edges_in(n);
+    for (int i = 0; i < edges_out.size(); i++) {
+        for (int j = 0; j < edges_out[i].size(); j++) {
+            edges_in[edges_out[i][j]].push_back(i);
+        }
+    }
+    _populate(edges_in, true, n);
+    _populate(edges_out, false, n);
+}
+
+void Graph::_populate(vector<vector<int>> & edges, bool in, int n) {
+    vector<int> local_offsets(n);
+    vector<int> local_edges;
+    local_offsets[0] = 0;
+    for (int i = 0; i < n; i++) {
+        int offset = edges[i].size();
+        if (i != (n-1))
+            local_offsets[i+1] = local_offsets[i]+offset;
+        for (int j = 0; j < offset; j++) {
+            local_edges.push_back(edges[i][j]);
+        }
+    }
+    
+    if (in) {
+        in_offsets = local_offsets;
+        in_edges = local_edges;
+    } else {
+        out_offsets = local_offsets;
+        out_edges = local_edges;
     }
 }
 
 int Graph::out_degree(int n) const {
     assert(n<this->num_nodes());
 
-    if (n == (this->num_nodes()-1)) return this->num_edges()-this->offsets[n];
-    return this->offsets[n+1] - this->offsets[n];
+    if (n == (this->num_nodes()-1)) return this->num_edges()-this->out_offsets[n];
+    return this->out_offsets[n+1] - this->out_offsets[n];
+}
+
+int Graph::in_degree(int n) const {
+    assert(n<this->num_nodes());
+
+    if (n == (this->num_nodes()-1)) return this->num_edges()-this->in_offsets[n];
+    return this->in_offsets[n+1] - this->in_offsets[n];
 }
 
 vector<int> Graph::out_neighbors(int n) const {
     assert(n<this->num_nodes());
 
-    auto begin = this->edges.cbegin()+this->offsets[n];
+    auto begin = this->out_edges.cbegin()+this->out_offsets[n];
     vector<int> neighbors(begin, begin+this->out_degree(n));
     return neighbors;
 }
@@ -36,18 +87,7 @@ vector<int> Graph::out_neighbors(int n) const {
 vector<int> Graph::in_neighbors(int n) const {
     assert(n<this->num_nodes());
 
-    vector<int> nodes; 
-    int current_node = 0;
-    int next_offset = (current_node == (this->num_nodes()-1) ? this->num_nodes() : this->offsets[current_node+1]);
-    for (int i = 0; i < this->num_edges(); i++) { 
-        // edges from current_node to this->edges[i]
-        while (i == next_offset) {
-            current_node += 1;
-            next_offset = (current_node == (this->num_nodes()-1) ? this->num_edges() : this->offsets[current_node+1]);
-        }
-        if (this->edges[i] == n) {
-            nodes.push_back(current_node);
-        }
-    }
-    return nodes;
+    auto begin = this->in_edges.cbegin()+this->in_offsets[n];
+    vector<int> neighbors(begin, begin+this->in_degree(n));
+    return neighbors;
 }
--- upcxx/graph.hpp
+++ upcxx/graph.hpp
@@ -9,16 +9,17 @@ using namespace upcxx;
 
 class Graph {
     using dobj_vector_t = dist_object<vector<int>>;
-    
-    private: 
-        void _populate(vector<vector<int>> & edges, bool in);
-        int _degree(vector<int> &offsets, const int n, const int total_edges);
-        vector<int> _neighbors(vector<int> &offsets, vector<int> &edges, int n);
-    public:
     dobj_vector_t in_offsets;
     dobj_vector_t in_edges;
     dobj_vector_t out_offsets;
     dobj_vector_t out_edges; 
+    private: 
+        void _populate(vector<vector<int>> & edges, bool in);
+        int _degree(vector<int> &offsets, const int n, const int total_edges);
+        vector<int> _neighbors(vector<int> &offsets, vector<int> &edges, int n);
+        
+    public:
+    
         int rank_start;
         int rank_end;
         int num_nodes;
