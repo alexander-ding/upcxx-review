81e081e2c8c50a152fce3530c214293601510c99
Alexander Ding <alexding123@outlook.com>
2019-08-11 16:36:37 -0400

    Fix

--- utils/GraphParser/__init__.py
+++ utils/GraphParser/__init__.py
@@ -2,6 +2,22 @@ from .social import *
 from .communities import *
 from .currency import *
 from .utils import mkdir_if_necessary
+import random
+
+def convert_to_weighted(in_p, out_p):
+    with open(in_p) as fin:
+        n = int(fin.readline()[:-1])
+        m = int(fin.readline()[:-1])
+        with open(out_p, 'w') as fout:
+            fout.write("{}\n".format(n))
+            fout.write("{}\n".format(m))
+            for _ in range(n):
+                fout.write(fin.readline())
+            
+            for _ in range(m):
+                to_index = int(fin.readline()[:-1])
+                weight = random.randint(0, 10)
+                fout.write("{} {}".format(to_index, weight))
 
 def get_all_graphs(config):
     from pathlib import Path
@@ -9,12 +25,14 @@ def get_all_graphs(config):
                TwitterCircleParser(config),
                GoogleCircleParser(config), 
                OrkutParser(config), 
-               YoutubeParser(config),
-               BitcoinOTCParser(config),
-               BitcoinAlphaParser(config)]
+               YoutubeParser(config)]
 
     mkdir_if_necessary(config.get("DEFAULT", "WorkPath"))
     mkdir_if_necessary(config.get("DEFAULT", "RealGraphPath"))
     for parser in parsers:
         parser.get()
         parser.cleanup()
+
+    for parser in parsers:
+        convert_to_weighted(parser.graph_path / parser.name, parser.graph_path.parent / "weighted" / parser.name)
+    
