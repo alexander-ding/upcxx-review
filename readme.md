a9f14d7dda5110cd2e9c9dac11268d35c1f98bd6
Alex Ding <alexding123@outlook.com>
2020-01-01 02:14:42 -0500

    Upload

---
+++ readme.md
@@ -0,0 +1,7 @@
+# An Evaluation of UPC++ by Porting Shared-Memory Parallel Algorithms
+
+Actually, I'm way too lazy to explain. 
+
+Paper is in the root directory. Go read it!
+
+Thank you MIT PRIMES. Thank you Yan. 
--- utils/archive/get_graphs.py
+++ utils/archive/get_graphs.py
@@ -6,7 +6,7 @@ import os
 cws = os.getcwd()
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if os.path.join(cws, "utils") != dir_path:
-    raise Exception("parse_graph.py must be run from the top-level directory of the repository")
+    raise Exception("get_graph.py must be run from the top-level directory of the repository")
     
 config = ConfigParser()
 config.read("config.ini")
